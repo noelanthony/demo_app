3ade384d0f55571770b2045f74cec2a6140073de
Noel-Anthony Apuya <noel.anthony@gmail.com>
2014-01-26 22:28:34 -0800

    Kestrel

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'cda4c71e08cae9bee6aa734f0abee585fca9b98353e4639832ae0ab9664867d009b0c587f398edb81b4cf3564235befe8e33b74b76d4535180a89de261cd3af1'
